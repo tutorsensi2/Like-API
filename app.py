b6f3c4865ee1608c45288a99731e6ce6ba765e21
tutorsensi2 <tutorsensi.1@gmail.com>
2026-02-03 14:46:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_headers(token):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB51",
+            "ReleaseVersion": "OB52",
         }
 
 def encrypt_message(data):
